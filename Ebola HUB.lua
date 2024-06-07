151c8a4e72d5a2dd1757cb5b1d8458c8f00d44ea
LZjH70ZwH1mKlcHWLpyAf <majs.machine+ebolahub@gmail.com>
2024-06-07 23:30:26 +0100

    Update Ebola HUB.lua

--- Ebola HUB.lua
+++ Ebola HUB.lua
@@ -129,13 +129,10 @@ local Button = MainTab:CreateButton({
 _G.infinjump = not _G.infinjump
 
 if _G.infinJumpStarted == nil then
-	--Ensures this only runs once to save resources
+
 	_G.infinJumpStarted = true
 	
-	--Notifies readiness
-	game.StarterGui:SetCore("SendNotification", {Title="Youtube Hub"; Text="Infinite Jump Activated!"; Duration=5;})
-
-	--The actual infinite jump
+	
 	local plr = game:GetService('Players').LocalPlayer
 	local m = plr:GetMouse()
 	m.KeyDown:connect(function(k)
